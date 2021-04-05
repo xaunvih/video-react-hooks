27c567519c5fd9350a10c1e0171d215dd1e2b495
vinhmai <vinhmai079@gmail.com>
2021-04-06 01:09:34 +0700

    apply styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import styled, { css } from 'styled-components'
 
-function App() {
-    return <h3>Hello Dude!</h3>
+interface IButtonProps {
+    primary: boolean
+}
+
+const Button = styled.button`
+    background: transparent;
+    border-radius: 3px;
+    border: 2px solid palevioletred;
+    color: palevioletred;
+    margin: 0 1em;
+    padding: 0.25em 1em;
+
+    ${(props: IButtonProps) =>
+        props.primary &&
+        css`
+            background: palevioletred;
+            color: white;
+        `};
+`
+
+function App(): JSX.Element {
+    return (
+        <div>
+            <Button primary={false}>Not Primary</Button>
+            <Button primary={true}>Primary</Button>
+            <h3>Hello Dude!</h3>
+        </div>
+    )
 }
 
 export default App
